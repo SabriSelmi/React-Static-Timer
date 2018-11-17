8783587662aa636a570d4df22bc09652285664d8
Sabri Selmi <43963123+SabriSelmi@users.noreply.github.com>
2018-11-17 02:49:02 +0100

    Update min.js

--- src/components/min.js
+++ src/components/min.js
@@ -1,6 +1,6 @@
 import React from "react";
 
 const Mintime =({aff})=>{
-    return (<div>{aff.minute<9? '0'+aff.minute+':' : aff.minute+':'}</div>)
+    return (<div>{aff.minute<9? '0'+aff.minute+' :' : aff.minute+' :'}</div>)
 }
 export default Mintime;
