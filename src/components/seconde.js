e6721c3156be4919a624ddaa017f861b6ac0d22e
Sabri Selmi <43963123+SabriSelmi@users.noreply.github.com>
2018-11-17 02:48:33 +0100

    Update seconde.js

--- src/components/seconde.js
+++ src/components/seconde.js
@@ -2,7 +2,7 @@ import React from "react";
 
 
     const Secondetime=({aff})=>{
-        return (<div>{aff.seconde<9? '0'+aff.seconde+':' : aff.seconde}</div>)
+        return (<div>{aff.seconde<9? '0'+aff.seconde : aff.seconde}</div>)
     }
 
 export default Secondetime;
