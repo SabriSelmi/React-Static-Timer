702e6cc028577697f035624f2fec4f3185234011
Sabri Selmi <43963123+SabriSelmi@users.noreply.github.com>
2018-11-17 02:49:28 +0100

    Update hour.js

--- src/components/hour.js
+++ src/components/hour.js
@@ -1,6 +1,6 @@
 import React from "react";
 
 const Hourtime=({aff})=>{
-    return (<div>{aff.hour<9? '0'+aff.hour+':' : aff.hour+':'}</div>)
+    return (<div>{aff.hour<9? '0'+aff.hour+' :' : aff.hour+' :'}</div>)
 }
 export default Hourtime;
